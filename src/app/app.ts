d085d14dadea8ea67a00429c9aa3b20b6b30d6ec
vmandic0 <v.mandic04@gmail.com>
2025-08-14 14:22:50 +0200

    Popravljen bag kod ucitavanja nabara duplo

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,25 @@
 import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Router, NavigationEnd, RouterOutlet } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { Navbar } from './shared/components/navbar/navbar';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true,
+  imports: [RouterOutlet, CommonModule, Navbar],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
   protected readonly title = signal('Podcastify');
+  protected readonly showNavbar = signal(true);
+
+  constructor(private router: Router) {
+    this.router.events.subscribe(event => {
+      if (event instanceof NavigationEnd) {
+        const url = event.urlAfterRedirects;
+        this.showNavbar.set(!(url.includes('/login') || url.includes('/register')));
+      }
+    });
+  }
 }
